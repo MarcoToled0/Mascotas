fb52d7fe7dc95e034a119fd20c743a7cec57a6fc
marco <marcotoledo380@gmail.com>
2022-10-30 09:03:30 -0300

    mascotas

---
+++ masc.js
@@ -0,0 +1,13 @@
+
+var mascota1 = "Pedrito"
+var mascota2 = "Rufo"
+var mascota3 = "Chancla"
+
+console.log("Mis mascotas son",mascota1,",", mascota2,"y", mascota3,". Les quiero mucho")
+
+console.log("Quiero mucho a mis mascotas",prompt("Dime el nombre de mascota1",mascota1,),"y", prompt("Dime el nombre de mascota2",mascota2))
+
+var variable = "100"
+console.log(variable, typeof variable)
+var variable = 100
+console.log(variable, typeof variable)
